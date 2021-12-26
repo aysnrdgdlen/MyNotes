16351f0d619f6265425e2d6b5e2fe378e22041ad
arifsamet12 <arifsametakkaya46@gmail.com>
2021-12-26 14:51:45 +0300

    NotesViewModel classı oluşturuldu.

---
+++ app/src/main/java/com/example/mynotes/ViewModel/NotesViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.mynotes.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.mynotes.Database.NotesDatabase
+import com.example.mynotes.Model.Notes
+import com.example.mynotes.Repository.NotesRepository
+
+class NotesViewModel(application: Application) : AndroidViewModel(application) {
+
+    val repository: NotesRepository
+
+    init {
+        val dao=NotesDatabase.getDatabaseInstance(application).myNotesDao()
+        repository=NotesRepository(dao)
+    }
+
+    fun addNotes(notes: Notes){
+        repository.insertNotes(notes)
+    }
+
+    fun getNotes():LiveData<List<Notes>> = repository.getAllNotes()
+
+    fun deleteNotes(id:Int){
+        repository.deleteNotes(id)
+    }
+
+    fun updateNotes(notes: Notes){
+        repository.updateNotes(notes)
+    }
+}
